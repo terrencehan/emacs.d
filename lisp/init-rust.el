ff14a06db3d6891945b05b0c04b90a87762f68af
terrencehan <isterrence@gmail.com>
2024-07-29 11:13:07 +0000

    disable eldoc for rust and add open all features

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -1,8 +1,24 @@
 ;;; init-rust.el --- Support for the Rust language -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;; Code:
+
+(use-package eglot
+  :config
+  (add-to-list 'eglot-server-programs
+               `(rust-mode . ("rust-analyzer" :initializationOptions
+                              (:procMacro (:enable t)
+                                          :cargo (:buildScripts (:enable t)
+                                                                :features
+                                                                "all")))))
+  )
+
+
 (use-package rust-mode
-  :hook (rust-mode . eglot-ensure))
+  :config
+  (add-hook 'eglot-managed-mode-hook (lambda () (eldoc-mode -1)))
+  :hook
+  (rust-mode . eglot-ensure))
+
 
 (provide 'init-rust)
 ;;; init-rust.el ends here
