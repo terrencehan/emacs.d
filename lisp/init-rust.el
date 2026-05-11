d595285c060259e921ee40d15ea82b7feaf6ad14
terrencehan <terrencehan@tencent.com>
2026-05-11 13:58:39 +0000

    update

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -4,6 +4,7 @@
 
 (use-package eglot
   :config
+  (setq eglot-ignored-server-capabilities '(:codeActionProvider))
   (add-to-list 'eglot-server-programs
                `(rust-mode . ("rust-analyzer" :initializationOptions
                               (:procMacro (:enable t)
@@ -19,22 +20,6 @@
   :hook
   (rust-mode . eglot-ensure))
 
-
-(with-eval-after-load 'flymake
-  ;; 1. 封死文本指示符：让它想画字符时画出空字符串
-  (setq flymake-note-indicator ""
-        flymake-warning-indicator ""
-        flymake-error-indicator ""
-        flymake-note-error-indicator ""
-        flymake-warning-error-indicator "")
-
-  ;; 2. 封死计数器：不让它显示 !!2
-  (setq flymake-suppress-zero-counters t)
-
-  ;; 3. 封死绘图逻辑：这是最暴力的，直接拦截它的绘图指令
-  ;; 无论它想在 Fringe 还是 Margin 画东西，都返回 nil
-  (advice-add 'flymake--fringe-overlay-spec :override (lambda (&rest _) nil))
-  (advice-add 'flymake--margin-overlay-spec :override (lambda (&rest _) nil)))
 
 
 (provide 'init-rust)
